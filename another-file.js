de6a53afaef77754e35de07f4599eaeded79f314
bobbyhadz <opmftw@gmail.com>
2024-03-02 10:35:14 +0200

    initial commit

---
+++ another-file.js
@@ -0,0 +1,5 @@
+// 👇️ named imports
+import {sum, num} from './index.js';
+
+console.log(sum(5, 5)); // 👉️ 10
+console.log(num); // 👉️ 100
---
+++ index.js
@@ -0,0 +1,9 @@
+// ReferenceError require is not defined
+
+// 👇️ named export
+export function sum(a, b) {
+  return a + b;
+}
+
+// 👇️ named export
+export const num = 100;
